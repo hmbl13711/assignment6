0a30bbd4509ddb984cf386c760368a100e752e10
Hafiz Bilal <hmbl13711@gmail.com>
2022-09-16 07:47:06 +0500

    assignment6

---
+++ app.js
@@ -0,0 +1,121 @@
+// List of loop programming exercises
+// 1. Write a ts program to print all natural numbers from 1 to n. - using while loop
+// var n:number=15
+// var index:number=1 
+// while(index<n){
+//     console.log(index)
+//     index++
+// }
+// 2. Write a ts program to print all natural numbers in reverse (from n to 1). - using while loop
+// var number:number=15
+// var index:number=0 
+// while(index<number){
+//     console.log(number);
+//     number--
+// }
+//3. Write a ts program to print all alphabets from a to z. - using while loop
+// var alpha:string[]=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// alpha.sort()
+// var index:number=0
+// while(index<alpha.length){
+//     console.log(alpha[index])
+//     index++
+// }
+// // 4. Write a ts program to print all even numbers between 1 to 100. - using while loop
+// var index:number=2 
+// while(index<101){
+//     console.log(index)
+//     index++;
+//     index++;
+// }
+// // 5. Write a ts program to print all odd number between 1 to 100.
+// var index:number=0
+// var num5:number=1
+// while(num5<100){
+//     console.log(num5)
+//     num5++;
+//     num5++;
+// }
+// 6. Write a ts program to find sum of all natural numbers between 1 to n.
+// var n:number=100
+// var index:number=0
+// var sum:number=((100+1)/2)
+// while (index<1) {
+//     console.log(sum*100)
+//     index++
+// }
+// 7. Write a ts program to find sum of all even numbers between 1 to n.
+// var n:number=100
+// var index:number=0
+// var evenNum:number=n/2
+// var formula:number=evenNum+1
+// while (index<1) {
+//     console.log((formula)*(evenNum))
+//     index++
+// }
+// // 8. Write a ts program to find sum of all odd numbers between 1 to n.
+// var n:number=100
+// var index:number=0
+// var oddNum:number=n/2
+// while (index<1) {
+//     console.log((oddNum)*(oddNum))
+//     index++
+// }
+// // 9. Write a ts program to print multiplication table of any number.
+// var n:number=2
+// var index:number=1
+// while(index<11){
+//     console.log(n*index)
+//     index++
+// } 
+// // 10. Write a ts program to count number of digits in a number.
+// var nu10:number[]=[56847]
+// var num10:any=nu10.toString()
+// var numb10:any=num10.split("")
+// var number:any=numb10.map(Number)
+// console.log(number.length)
+// // 11. Write a ts program to find first and last digit of a number.
+// var num11:number[]=[56783]
+// var numb11:any=num10.toString().split("")
+// var nu11:number=numb10.map(Number)
+// console.log(nu11[0],"first digit of number")
+// console.log(nu11[4],"first digit of number")
+// 12. Write a ts program to find sum of first and last digit of a number.
+// var num12:number[]=[56783]
+// var numb12:any=num12.toString().split("")
+// var nu12:number=numb12.map(Number)
+// var sumOfNum:number=nu12[0]+nu12[4]
+// console.log("sum of first and last digit of number is",sumOfNum)
+// 13. Write a ts program to swap first and last digits of a number.
+// var nu13:number[]=[12456]
+// var num13:any=nu13.toString().split("")
+// var numb13:any=num13.map(Number)
+// numb13.splice(0,1,6)
+// numb13.splice(4,1,1)
+// console.log(numb13)
+// 14. Write a ts program to calculate sum of digits of a number.
+// 15. Write a ts program to calculate product of digits of a number.
+// 16. Write a ts program to enter a number and print its reverse.
+var num16 = [145782];
+console.log(num16.reverse);
+// var numb16:any=num16.toString().split("")
+// var number16:any=num16.map(Number).reverse
+// 17. Write a ts program to check whether a number is palindrome or not.
+// 18. Write a ts program to find frequency of each digit in a given integer.
+// 19. Write a ts program to enter a number and print it in words.
+// 20. Write a ts program to print all ASCII character with their values.
+// 21. Write a ts program to find power of a number using for loop.
+// 22. Write a ts program to find all factors of a number.
+// 23. Write a ts program to calculate factorial of a number.
+// 24. Write a ts program to find HCF (GCD) of two numbers.
+// 25. Write a ts program to find LCM of two numbers.
+// 26. Write a ts program to check whether a number is Prime number or not.
+// 27. Write a ts program to print all Prime numbers between 1 to n.
+// 28. Write a ts program to find sum of all prime numbers between 1 to n.
+// 29. Write a ts program to find all prime factors of a number.
+// 30. Write a ts program to check whether a number is Armstrong number or not.
+// 31. Write a ts program to print all Armstrong numbers between 1 to n.
+// 32. Write a ts program to check whether a number is Perfect number or not.
+// 33. Write a ts program to print all Perfect numbers between 1 to n.
+// 34. Write a ts program to check whether a number is Strong number or not.
+// 35. Write a ts program to print all Strong numbers between 1 to n.
